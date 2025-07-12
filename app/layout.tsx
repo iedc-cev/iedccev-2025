eb450a732f8669e148ee42ca7ad2dd9fbd55232e
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-07-12 17:30:47 +0530

    rger

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Navbar from "@/components/navbar"
-import Footer from "@/components/Footer"
+import Footer from "@/components/footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
