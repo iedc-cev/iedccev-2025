de0762e78dcbb599fac3077b8e6cd2fa6149207d
Abhin <abhinchelakkal@gmail.com>
2025-07-25 22:02:51 +0530

    asdfds

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
   const pathname = usePathname()
 
   // Hide footer on /join
-  if (pathname === '/eceee' || pathname === '/town') return null
+  if (pathname === '/raspberrypi' || pathname === '/town') return null
   return (
     <nav className="w-full bg-white/95 backdrop-blur-sm border-b border-gray-100 z-50">
       <div className="max-w-7xl mx-auto px-4 py-2 sm:px-6 lg:px-8">
