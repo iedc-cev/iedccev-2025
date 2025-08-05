3558cb823c2b99bd280d7cb8acd838709f531668
Amarnath  Sooraj <166597458+AmarnathSooraj@users.noreply.github.com>
2025-08-05 13:17:42 +0530

    Update staticEvents.ts

--- components/staticEvents.ts
+++ components/staticEvents.ts
@@ -8,7 +8,7 @@ export const staticEvents = [
     venue: "Asap Room,CEV",
     poster_url: "/posters/Xylem.jpg",
     is_live: true,
-    form_active: true,
+    form_active: false,
     registration_link: "https://www.iedccev.org/smash",
     aftermovie_link: "",
   },
