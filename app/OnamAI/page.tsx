378d5b4bd60741bf24461a50e3df96364e6923dc
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-08-24 10:58:49 +0530

    dss

--- app/OnamAI/page.tsx
+++ app/OnamAI/page.tsx
@@ -3,23 +3,25 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="relative min-h-screen w-full bg-[">
+    <div className="relative min-h-screen w-full flex justify-center">
       {/* Background Image */}
-      <div className="absolute inset-0">
-        <Image
-          src="/posters/OnamAI.jpg"
-          alt="Background"
-          fill
-          className="object-cover brightness-50"
-          priority
-        />
+      <div className="absolute inset-0 flex justify-center">
+        <div className="w-full min-w-4xl relative">
+          <Image
+            src="/posters/OnamAI.jpg"
+            alt="Background"
+            fill
+            className="object-center brightness-50 lg:object-cover"
+            priority
+          />
+        </div>
       </div>
 
-      {/* Centered Content */}
-      <div className="relative z-10 flex flex-col items-center justify-end min-h-[80vh]">
+      {/* Content */}
+      <div className="relative z-10 flex items-end justify-center min-h-screen pb-10">
         <a
           href="https://forms.gle/ryZ3ScEwUqAEzyxr5"
-          className="bg-white px-6 py-3 rounded-lg shadow-md text-black font-medium hover:bg-[#b6b5b5] hover:scale-95 active:scale-105 transition-all duration-100"
+          className="bg-white p-3 flex items-center justify-center rounded-lg shadow-md text-black font-medium text-md hover:bg-[#b6b5b5] hover:scale-95 active:scale-105 transition-all duration-100"
         >
           Register Now
         </a>
