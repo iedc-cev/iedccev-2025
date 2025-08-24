dea3661f10307c5de1be4514f9469b4c2d744382
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-08-24 11:38:59 +0530

    added

--- app/OnamAI/page.tsx
+++ app/OnamAI/page.tsx
@@ -1,31 +1,25 @@
 import React from "react";
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge"
+import { Calendar, MapPin, Clock, ExternalLink } from "lucide-react"
 
 export default function Page() {
   return (
-    <div className="relative min-h-screen w-full flex justify-center">
-      {/* Background Image */}
-      <div className="absolute inset-0 flex justify-center">
-        <div className="w-full min-w-4xl relative">
-          <Image
-            src="/posters/OnamAI.jpg"
-            alt="Background"
-            fill
-            className="object-center brightness-50 lg:object-cover"
-            priority
-          />
+    <div className="relative min-h-screen w-full bg-[#c09f45] justify-center items-center flex flex-col px-3">
+    <div className="rounded-sm overflow-hiddentransition-shadow max-h-[900px] flex flex-col justify-between items-center">
+        <div className=" aspect-auto relative flex max-w-4xl h-[500px]">
+            <Image
+            src="/posters/onam.jpg"
+            alt="rgerg"
+            width={200}
+            height={600}
+            className="object-cover w-full rounded-lg"
+            />
         </div>
-      </div>
-
-      {/* Content */}
-      <div className="relative z-10 flex items-end justify-center min-h-screen pb-10">
-        <a
-          href="https://forms.gle/ryZ3ScEwUqAEzyxr5"
-          className="bg-white p-3 flex items-center justify-center rounded-lg shadow-md text-black font-medium text-md hover:bg-[#b6b5b5] hover:scale-95 active:scale-105 transition-all duration-100"
-        >
-          Register Now
-        </a>
-      </div>
+        <a href="https://forms.gle/ryZ3ScEwUqAEzyxr5" className="w-full flex items-center justify-center bg-white my-5 py-3 rounded-lg ">Submit Now</a>
+    </div>
     </div>
   );
 }
