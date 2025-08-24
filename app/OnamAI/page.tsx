495c960df0930b4856907a691f13e77e11ac9263
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-08-24 10:15:11 +0530

    added

---
+++ app/OnamAI/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+export default function page() {
+  return (
+        <div className="relative min-h-screen w-full">
+          {/* Background Image */}
+          <Image
+            src="/posters/OnamAI.jpg" // path inside public/posters/
+            alt="Background"
+            fill   // makes the image cover the parent div
+            className="object-cover brightness-50"
+            priority
+          />
+    
+          <div className="relative z-10 flex flex-col items-center justify-end min-h-[600px]">
+            <a
+              href="https://forms.gle/ryZ3ScEwUqAEzyxr5"
+              className="bg-white px-6 py-2 text-md rounded-lg shadow-md text-black font-medium hover:bg-[#b6b5b5] hover:scale-95 active:scale-102 transition-all duration-50"
+            >
+              Submit Now
+            </a>
+          </div>
+        </div>
+  )
+}
