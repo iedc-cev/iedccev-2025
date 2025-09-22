fb863d7141ef53322b7f182f6daabea853f16463
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-09-22 16:46:50 +0530

    added

---
+++ app/vibeCoding/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {redirect} from 'next/navigation'
+
+function page() {
+    redirect('https://snaptiqz.com/event/preview/qj6s25jzb7px4m7wdrafg8et')
+}
+
+export default page
