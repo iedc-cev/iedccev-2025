9815ccef8bbd81354147a90c33a89061cee58553
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-09-22 21:15:35 +0530

    add

---
+++ app/joinIEDCnetwork/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {redirect} from 'next/navigation'
+
+function page() {
+    redirect('https://docs.google.com/forms/d/e/1FAIpQLSelHbI4I12mE-mQBLU-LRn6bG5EHLBuvQA1k_V7DyAVeFKP2Q/viewform');
+}
+
+export default page
