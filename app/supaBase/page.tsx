c6373f4937936f16fbd2d73a154de622e6231cfd
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-12-15 10:38:33 +0530

    make changes

--- app/supaBase/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { redirect } from 'next/navigation'
-
-function page() {
-    redirect('https://snaptiqz.com/event/_5BE-U8YLxRYgBKU11SJ4')
-}
-
-export default page
--- app/vibeCoding/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {redirect} from 'next/navigation'
-
-function page() {
-    redirect('https://snaptiqz.com/event/GuXzrJzFz0Uc06Us0335U')
-}
-
-export default page
